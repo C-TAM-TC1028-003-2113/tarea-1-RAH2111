29dfc70fdedb83ab40ae0f2ff216da5a39e3431b
6QW95LA_1909 <a01411863@itesm.mx>
2021-08-31 12:02:43 -0500

    Levadura corregido

--- assignments/21LevaduraParaPizza/src/exercise.py
+++ assignments/21LevaduraParaPizza/src/exercise.py
@@ -1,9 +1,9 @@
 def main():
     # escribe tu código abajo de esta línea
     harina = float(input("Dame la harina en gramos: "))
-    kilos = harina / 1000
+    levadura = harina * 50 / 1000
 
-    print("Necesitas estos gramos de levadura:", kilos * 50)
+    print("Necesitas estos gramos de levadura:", levadura)
 
 
 if __name__ == '__main__':
