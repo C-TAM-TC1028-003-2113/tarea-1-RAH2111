b4ee050918879ba1968c0b1d9f47ce231f9c8f60
6QW95LA_1909 <a01411863@itesm.mx>
2021-08-31 11:56:35 -0500

    Videojuegos corregido

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -3,7 +3,7 @@ def main():
     nuevos = int(input("Dame la cantidad de juegos nuevos: "))
     usados = int(input("Dame la cantidad de juegos usados: "))
 
-    print("El total de la compra es: ", (nuevos * 1000 + usados * 350))
+    print("El total de la compra es:", (nuevos * 1000 + usados * 350))
 
 
 if __name__ == '__main__':
